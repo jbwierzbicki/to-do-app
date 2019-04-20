098fa077b10f9bd486651e87f1e48c27a4ebcff1
Johnathan <jbwierzbicki@gmail.com>
2019-04-20 12:44:29 -0400

    assignment 1 - delete btn listener

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function onReady() {
-    const toDos = [];
+    var toDos = [];
     const addToDoForm = document.getElementById('addToDoForm');
+    let id = 0;
 
     function createNewToDo() {
         const newToDoText = document.getElementById('newToDoText');
@@ -8,10 +9,13 @@ function onReady() {
 
         toDos.push({
             title: newToDoText.value,
-            complete: false
+            complete: false,
+            id: id
         });
         newToDoText.value = '';
 
+        console.log(toDos);
+        id++;
         renderTheUI();
     }
 
@@ -21,14 +25,34 @@ function onReady() {
         toDoList.textContent = '';
 
         toDos.forEach(function (toDo) {
+
             const newLi = document.createElement('li');
             const checkbox = document.createElement('input');
+            const deleteBtn = document.createElement('input');
+
+            deleteBtn.type = "button";
             checkbox.type = "checkbox";
 
             newLi.textContent = toDo.title;
+            deleteBtn.value = "Delete";
 
             toDoList.appendChild(newLi);
             newLi.appendChild(checkbox);
+            newLi.appendChild(deleteBtn);
+
+
+            deleteBtn.addEventListener('click', function () {
+                const filtered = toDos.filter(toDos => toDo.id !== toDos.id);
+                console.log(filtered);
+
+                toDos = filtered;
+                                
+                newLi.textContent = '';
+
+                renderTheUI()
+            });
+
+
         });
     }
 
@@ -36,8 +60,11 @@ function onReady() {
         event.preventDefault();
         createNewToDo();
     });
-    renderTheUI();
+
 }
+
+
+
 
 window.onload = function () {
     onReady();
